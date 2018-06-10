45b23c3688ba295424abd65758cf2769758ce72c
Alex Korzh <alexkorzh7@gmail.com>
2018-06-10 18:52:31 +0300

    Code refactoring and memory clean-up on app exit

--- SimpleHandTrackerMac/SimpleHandTracker/SimpleHandTrackerSource/main.cpp
+++ SimpleHandTrackerMac/SimpleHandTracker/SimpleHandTrackerSource/main.cpp
@@ -30,18 +30,12 @@ enum TrackerState
 };
 
 /* Global Variables  */
-int _fontFace = FONT_HERSHEY_PLAIN;
-int _squareLength;
-int avgColor[NSAMPLES][3];
+const int _fontFace = FONT_HERSHEY_PLAIN;
+const int _squareLength = 20;
+int _averageColors[NSAMPLES][3];
 int c_lower[NSAMPLES][3];
 int c_upper[NSAMPLES][3];
-int avgBGR[3];
-int nrOfDefects;
-struct BoundingDimensions { int w; int h; } boundingDim;
-Mat edges;
-vector <ColorSampleROI> colorSampleRegions;
-vector <KalmanFilter> kf;
-vector <Mat_<float>> measurement;
+vector <ColorSampleROI> _colorSampleRegions;
 
 ImageSource _imageSource(0);
 TrackerState _currentState;
@@ -49,7 +43,13 @@ HandGesture _handGesture;
 
 /* end global variables */
 
-int getMedian(vector<int> val) {
+template <std::size_t sizeA, std::size_t sizeB>
+void clearArray(int (&a)[sizeA][sizeB]) {
+    int* begin = &a[0][0];
+    std::fill_n(begin, sizeA * sizeB, 0.0);
+}
+
+int GetMedian(vector<int> val) {
 	int median;
 	size_t size = val.size();
 	sort(val.begin(), val.end());
@@ -63,7 +63,7 @@ int getMedian(vector<int> val) {
 }
 
 
-void getAvgColor(ColorSampleROI roi, int avg[3]) {
+void GetAverageColor(ColorSampleROI roi, int avg[3]) {
 	Mat r;
 	roi.roi_ptr.copyTo(r);
 	vector<int>hm;
@@ -77,12 +77,12 @@ void getAvgColor(ColorSampleROI roi, int avg[3]) {
 			lm.push_back(r.data[r.channels()*(r.cols*i + j) + 2]);
 		}
 	}
-	avg[0] = getMedian(hm);
-	avg[1] = getMedian(sm);
-	avg[2] = getMedian(lm);
+	avg[0] = GetMedian(hm);
+	avg[1] = GetMedian(sm);
+	avg[2] = GetMedian(lm);
 }
 
-void normalizeColors() {
+void NormalizeColors() {
 	// copy all boundaries read from trackbar
 	// to all of the different boundries
 	for (int i = 1; i < NSAMPLES; i++) {
@@ -94,42 +94,42 @@ void normalizeColors() {
 	// normalize all boundries so that 
 	// threshold is whithin 0-255
 	for (int i = 0; i < NSAMPLES; i++) {
-		if ((avgColor[i][0] - c_lower[i][0]) < 0) {
-			c_lower[i][0] = avgColor[i][0];
-		}if ((avgColor[i][1] - c_lower[i][1]) < 0) {
-			c_lower[i][1] = avgColor[i][1];
-		}if ((avgColor[i][2] - c_lower[i][2]) < 0) {
-			c_lower[i][2] = avgColor[i][2];
-		}if ((avgColor[i][0] + c_upper[i][0]) > 255) {
-			c_upper[i][0] = 255 - avgColor[i][0];
-		}if ((avgColor[i][1] + c_upper[i][1]) > 255) {
-			c_upper[i][1] = 255 - avgColor[i][1];
-		}if ((avgColor[i][2] + c_upper[i][2]) > 255) {
-			c_upper[i][2] = 255 - avgColor[i][2];
+		if ((_averageColors[i][0] - c_lower[i][0]) < 0) {
+			c_lower[i][0] = _averageColors[i][0];
+		} if ((_averageColors[i][1] - c_lower[i][1]) < 0) {
+			c_lower[i][1] = _averageColors[i][1];
+		} if ((_averageColors[i][2] - c_lower[i][2]) < 0) {
+			c_lower[i][2] = _averageColors[i][2];
+		} if ((_averageColors[i][0] + c_upper[i][0]) > 255) {
+			c_upper[i][0] = 255 - _averageColors[i][0];
+		} if ((_averageColors[i][1] + c_upper[i][1]) > 255) {
+			c_upper[i][1] = 255 - _averageColors[i][1];
+		} if ((_averageColors[i][2] + c_upper[i][2]) > 255) {
+			c_upper[i][2] = 255 - _averageColors[i][2];
 		}
 	}
 }
 
-void produceBinaries(ImageSource *m) {
+void ProduceBinaries(ImageSource *imageSrc) {
 	Scalar lowerBound;
 	Scalar upperBound;
 	for (int i = 0; i < NSAMPLES; i++) {
-		normalizeColors();
-		lowerBound = Scalar(avgColor[i][0] - c_lower[i][0], avgColor[i][1] - c_lower[i][1], avgColor[i][2] - c_lower[i][2]);
-		upperBound = Scalar(avgColor[i][0] + c_upper[i][0], avgColor[i][1] + c_upper[i][1], avgColor[i][2] + c_upper[i][2]);
-		m->bwList.push_back(Mat(m->downsampled.rows, m->downsampled.cols, CV_8U));
-		inRange(m->downsampled, lowerBound, upperBound, m->bwList[i]);
+		NormalizeColors();
+		lowerBound = Scalar(_averageColors[i][0] - c_lower[i][0], _averageColors[i][1] - c_lower[i][1], _averageColors[i][2] - c_lower[i][2]);
+		upperBound = Scalar(_averageColors[i][0] + c_upper[i][0], _averageColors[i][1] + c_upper[i][1], _averageColors[i][2] + c_upper[i][2]);
+		imageSrc->bwList.push_back(Mat(imageSrc->downsampled.rows, imageSrc->downsampled.cols, CV_8U));
+		inRange(imageSrc->downsampled, lowerBound, upperBound, imageSrc->bwList[i]);
 	}
-	m->bwList[0].copyTo(m->binary);
+	imageSrc->bwList[0].copyTo(imageSrc->binary);
 	for (int i = 1; i < NSAMPLES; i++) {
-		m->binary += m->bwList[i];
+		imageSrc->binary += imageSrc->bwList[i];
 	}
-	medianBlur(m->binary, m->binary, 7);
+	medianBlur(imageSrc->binary, imageSrc->binary, 7);
 }
 
-int findBiggestContour(vector<vector<Point> > contours) {
+int FindBiggestContour(vector<vector<Point> > contours) {
 	int indexOfBiggestContour = -1;
-	int sizeOfBiggestContour = 0;
+	unsigned long sizeOfBiggestContour = 0;
 	for (int i = 0; i < contours.size(); i++) {
 		if (contours[i].size() > sizeOfBiggestContour) {
 			sizeOfBiggestContour = contours[i].size();
@@ -139,12 +139,11 @@ int findBiggestContour(vector<vector<Point> > contours) {
 	return indexOfBiggestContour;
 }
 
-void myDrawContours(ImageSource *m, HandGesture *hg) {
+void DrawHandContours(ImageSource *m, HandGesture *hg) {
 	drawContours(m->original, hg->hullPoint, hg->cIdx, cv::Scalar(200, 0, 0), 2, 8, vector<Vec4i>(), 0, Point());
 
 	rectangle(m->original, hg->bRect.tl(), hg->bRect.br(), Scalar(0, 0, 200));
 	vector<Vec4i>::iterator d = hg->defects[hg->cIdx].begin();
-
 
 	vector<Mat> channels;
 	Mat result;
@@ -156,23 +155,26 @@ void myDrawContours(ImageSource *m, HandGesture *hg) {
 
 	while (d != hg->defects[hg->cIdx].end()) {
 		Vec4i& v = (*d);
-		int startidx = v[0]; Point ptStart(hg->contours[hg->cIdx][startidx]);
-		int endidx = v[1]; Point ptEnd(hg->contours[hg->cIdx][endidx]);
-		int faridx = v[2]; Point ptFar(hg->contours[hg->cIdx][faridx]);
-		float depth = v[3] / 256;
+		int startidx = v[0];
+        Point ptStart(hg->contours[hg->cIdx][startidx]);
+		int endidx = v[1];
+        Point ptEnd(hg->contours[hg->cIdx][endidx]);
+		int faridx = v[2];
+        Point ptFar(hg->contours[hg->cIdx][faridx]);
+//        float depth = v[3] / 256;
 		circle(result, ptFar, 9, Scalar(0, 205, 0), 5);
 
 		d++;
 	}
 }
 
-void makeContours(ImageSource *m, HandGesture* hg) {
+void MakeContours(ImageSource *m, HandGesture* hg) {
 	Mat aBw;
 	pyrUp(m->binary, m->binary);
 	m->binary.copyTo(aBw);
 	findContours(aBw, hg->contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
 	hg->initVectors();
-	hg->cIdx = findBiggestContour(hg->contours);
+	hg->cIdx = FindBiggestContour(hg->contours);
 	if (hg->cIdx != -1) {
 		hg->bRect = boundingRect(Mat(hg->contours[hg->cIdx]));
 		convexHull(Mat(hg->contours[hg->cIdx]), hg->hullPoint[hg->cIdx], false, true);
@@ -187,7 +189,7 @@ void makeContours(ImageSource *m, HandGesture* hg) {
 		if (isHand) {
 			hg->getFingertips(m);
 			hg->drawFingertips(m);
-			myDrawContours(m, hg);
+			DrawHandContours(m, hg);
 		}
 	}
 }
@@ -195,14 +197,12 @@ void makeContours(ImageSource *m, HandGesture* hg) {
 extern "C" int Init(int& outCameraWidth, int& outCameraHeight)
 {
 	_imageSource = ImageSource(0);
-	_currentState = NONE;
-
+    _currentState = NONE;
+    
 	if (!_imageSource.capture.isOpened())
 	{
 		return 1;
 	}
-
-	_squareLength = 20;
 
 	outCameraWidth = _imageSource.capture.get(CAP_PROP_FRAME_WIDTH);
 	outCameraHeight = _imageSource.capture.get(CAP_PROP_FRAME_HEIGHT);
@@ -213,15 +213,15 @@ extern "C" int Init(int& outCameraWidth, int& outCameraHeight)
 extern "C" void SetupForGettingColorSample()
 {
 	_imageSource.capture >> _imageSource.original;
-
+    
 	ImageSource* imageSrc = &_imageSource;
-	colorSampleRegions.push_back(ColorSampleROI(Point(imageSrc->original.cols / 3, imageSrc->original.rows / 6), Point(imageSrc->original.cols / 3 + _squareLength, imageSrc->original.rows / 6 + _squareLength), imageSrc->original));
-	colorSampleRegions.push_back(ColorSampleROI(Point(imageSrc->original.cols / 4, imageSrc->original.rows / 2), Point(imageSrc->original.cols / 4 + _squareLength, imageSrc->original.rows / 2 + _squareLength), imageSrc->original));
-	colorSampleRegions.push_back(ColorSampleROI(Point(imageSrc->original.cols / 3, imageSrc->original.rows / 1.5), Point(imageSrc->original.cols / 3 + _squareLength, imageSrc->original.rows / 1.5 + _squareLength), imageSrc->original));
-	colorSampleRegions.push_back(ColorSampleROI(Point(imageSrc->original.cols / 2, imageSrc->original.rows / 2), Point(imageSrc->original.cols / 2 + _squareLength, imageSrc->original.rows / 2 + _squareLength), imageSrc->original));
-	colorSampleRegions.push_back(ColorSampleROI(Point(imageSrc->original.cols / 2.5, imageSrc->original.rows / 2.5), Point(imageSrc->original.cols / 2.5 + _squareLength, imageSrc->original.rows / 2.5 + _squareLength), imageSrc->original));
-	colorSampleRegions.push_back(ColorSampleROI(Point(imageSrc->original.cols / 2, imageSrc->original.rows / 1.5), Point(imageSrc->original.cols / 2 + _squareLength, imageSrc->original.rows / 1.5 + _squareLength), imageSrc->original));
-	colorSampleRegions.push_back(ColorSampleROI(Point(imageSrc->original.cols / 2.5, imageSrc->original.rows / 1.8), Point(imageSrc->original.cols / 2.5 + _squareLength, imageSrc->original.rows / 1.8 + _squareLength), imageSrc->original));
+	_colorSampleRegions.push_back(ColorSampleROI(Point(imageSrc->original.cols / 3, imageSrc->original.rows / 6), Point(imageSrc->original.cols / 3 + _squareLength, imageSrc->original.rows / 6 + _squareLength), imageSrc->original));
+	_colorSampleRegions.push_back(ColorSampleROI(Point(imageSrc->original.cols / 4, imageSrc->original.rows / 2), Point(imageSrc->original.cols / 4 + _squareLength, imageSrc->original.rows / 2 + _squareLength), imageSrc->original));
+	_colorSampleRegions.push_back(ColorSampleROI(Point(imageSrc->original.cols / 3, imageSrc->original.rows / 1.5), Point(imageSrc->original.cols / 3 + _squareLength, imageSrc->original.rows / 1.5 + _squareLength), imageSrc->original));
+	_colorSampleRegions.push_back(ColorSampleROI(Point(imageSrc->original.cols / 2, imageSrc->original.rows / 2), Point(imageSrc->original.cols / 2 + _squareLength, imageSrc->original.rows / 2 + _squareLength), imageSrc->original));
+	_colorSampleRegions.push_back(ColorSampleROI(Point(imageSrc->original.cols / 2.5, imageSrc->original.rows / 2.5), Point(imageSrc->original.cols / 2.5 + _squareLength, imageSrc->original.rows / 2.5 + _squareLength), imageSrc->original));
+	_colorSampleRegions.push_back(ColorSampleROI(Point(imageSrc->original.cols / 2, imageSrc->original.rows / 1.5), Point(imageSrc->original.cols / 2 + _squareLength, imageSrc->original.rows / 1.5 + _squareLength), imageSrc->original));
+	_colorSampleRegions.push_back(ColorSampleROI(Point(imageSrc->original.cols / 2.5, imageSrc->original.rows / 1.8), Point(imageSrc->original.cols / 2.5 + _squareLength, imageSrc->original.rows / 1.8 + _squareLength), imageSrc->original));
 	_currentState = AWAITNG_PALM;
 }
 
@@ -244,8 +244,8 @@ extern "C" void StartTracking() {
 void GetColorSample(ImageSource *imageSrc) {
     cvtColor(imageSrc->original, imageSrc->original, ORIGCOL2COL);
     for (int j = 0; j < NSAMPLES; j++) {
-        getAvgColor(colorSampleRegions[j], avgColor[j]);
-        colorSampleRegions[j].draw_rectangle(imageSrc->original);
+        GetAverageColor(_colorSampleRegions[j], _averageColors[j]);
+        _colorSampleRegions[j].draw_rectangle(imageSrc->original);
     }
     cvtColor(imageSrc->original, imageSrc->original, COL2ORIGCOL);
     string imgText = string("Finding average color of hand");
@@ -257,9 +257,9 @@ void Tracking(ImageSource imageSrc) {
     pyrDown(imageSrc.original, imageSrc.downsampled);
     blur(imageSrc.downsampled, imageSrc.downsampled, Size(3, 3));
     cvtColor(imageSrc.downsampled, imageSrc.downsampled, ORIGCOL2COL);
-    produceBinaries(&imageSrc);
+    ProduceBinaries(&imageSrc);
     cvtColor(imageSrc.downsampled, imageSrc.downsampled, COL2ORIGCOL);
-    makeContours(&imageSrc, &_handGesture);
+    MakeContours(&imageSrc, &_handGesture);
     _handGesture.getFingerNumber(&imageSrc);
     pyrDown(imageSrc.binary, imageSrc.binary);
     pyrDown(imageSrc.binary, imageSrc.binary);
@@ -275,7 +275,7 @@ void Tracking(ImageSource imageSrc) {
 
 void DrawColorSampleRegions(ImageSource *imageSrc) {
 	for (int i = 0; i < NSAMPLES; i++) {
-		colorSampleRegions[i].draw_rectangle(_imageSource.original);
+		_colorSampleRegions[i].draw_rectangle(_imageSource.original);
 	}
 	string imgText = string("Cover rectangles with palm");
 	putText(imageSrc->original, imgText, Point(imageSrc->original.cols / 2, imageSrc->original.rows / 10), _fontFace, 1.2f, Scalar(200, 0, 0), 2);
@@ -319,5 +319,10 @@ extern "C" void  GetFrame(PixelData* pixels)
 
 extern "C" void  Close()
 {
+    _currentState = NONE;
+    clearArray(c_lower);
+    clearArray(c_upper);
+    clearArray(_averageColors);
+    _colorSampleRegions.clear();
 	_imageSource.capture.release();
 }
